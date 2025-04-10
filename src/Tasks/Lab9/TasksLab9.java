898e843b8aaa5ec43501f53a80f4881229886f58
Arttero <artt3ro@gmail.com>
2025-04-10 08:58:17 +0200

    Dodanie laba 8 i 9

---
+++ src/Tasks/Lab9/TasksLab9.java
@@ -0,0 +1,21 @@
+package Tasks.Lab9;
+
+public class TasksLab9 {
+
+
+    public void Task01(){
+
+    }
+    public void Task02(){
+
+    }
+    public void Task03(){
+
+    }
+    public void Task04(){
+
+    }
+    public void Task05(){
+
+    }
+}
