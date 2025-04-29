2bf64d494889091a74f2989da068bc85441153a0
Arttero <artt3ro@gmail.com>
2025-04-29 14:07:20 +0200

    Poprawki w zadaniu 1 i dodanie zadania 2 z LAB 10

--- src/Tasks/Lab10/TasksLab10.java
+++ src/Tasks/Lab10/TasksLab10.java
@@ -1,30 +1,14 @@
 package Tasks.Lab10;
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Scanner;
+import Tasks.Lab10.ZAD1.ZliczanieSlow;
+import Tasks.Lab10.ZAD2.SystemLogowania;
 
 public class TasksLab10 {
     public void Task01(){
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Podaj tekst:");
-        String tekst = sc.nextLine();
-        //wykorzystywanie regexa do pozbycia sie duzych liter i znakow specialnych
-        tekst = tekst.toLowerCase().replaceAll("[.,!?;:]","");
-
-        String[] words = tekst.split("\\s+"); //pozbycie sie bialych znakow
-        Map<String, Integer> wordMap = new HashMap<>();
-        for (String word : words) {
-            wordMap.put(word, wordMap.getOrDefault(word, 0) + 1);
-        }
-        System.out.println("Lista wystąpień: słów:\n");
-        for(Map.Entry<String, Integer> entry : wordMap.entrySet()) {
-            System.out.println(entry.getKey() + ": " + entry.getValue());
-        }
-
+        new ZliczanieSlow().PodawanieZdania();
     }
     public void Task02(){
-
+        new SystemLogowania().MenuLogowania();
     }
     public void Task03(){
 
